d9b851843bcb09b21c5b312fb5dc5b3693d0b823
jamesborn89 <jamesborn89@gmail.com>
2020-08-24 10:34:45 +0400

    new project

---
+++ app/js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+
+    // burger
+    // $('.header__menu-burger, .header__menu-list').on('click', function(){
+    //   $('.header__menu-burger').toggleClass('active');
+    // });
+  
+    // $('.header__menu-burger, .header__menu-list').click( function(){
+    //     $('.header__menu-list').slideToggle();
+    //   });
+  
+  
+    });
